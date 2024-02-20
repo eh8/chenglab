79df5047361aa5982c0ee242cff413f1bc31c6b0
Eric Cheng <eric@chengeric.com>
2024-02-20 07:36:23 -0500

    Even more home-manager stuff

--- modules/home.nix
+++ modules/home.nix
@@ -21,18 +21,44 @@
     };
   };
 
-  programs.zsh = {
+  programs.zsh = {  
     enable = true;
     enableCompletion = true;
     enableAutosuggestions = true;
     syntaxHighlighting = { 
       enable = true;
     };
-    prezto = {
-      enable = true;
-      tmux.autoStartRemote = true;
-      tmux.autoStartLocal = true;
+    shellAliases = {  
+      cat = "bat --style=plain --theme=base16 --paging=never ";
+      ls = "eza ";
+      e = "eza ";
+      v = "nvim ";
+      sudo = "sudo ";
+      ".." = "cd ..";
     };
+    # https://discourse.nixos.org/t/zsh-zplug-powerlevel10k-zshrc-is-readonly/30333
+    initExtra = ''
+      if [ -z "$TMUX" ]; then
+        exec tmux new-session -A -s 0
+      fi
+
+      fortune
+
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+    '';
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+  };
+
+  programs.neovim = {
+    enable = true;
+    viAlias = true;
+    vimAlias = true;
   };
 
   programs.fzf = {
@@ -44,6 +70,8 @@
     enable = true;
     clock24 = true;
     mouse = true;
+    terminal = "screen-256color";
+    baseIndex = 1;
   };
 
   programs.ssh = {
@@ -61,14 +89,19 @@
     homeDirectory = "/home/eh8";
     stateVersion = "23.11";
     packages = [
+      pkgs.bat
       pkgs.btop
       pkgs.croc
       pkgs.duf
-      pkgs.exa
+      pkgs.eza
+      pkgs.fortune-kind
       pkgs.just
       pkgs.kopia
       pkgs.neofetch
       pkgs.tealdeer
     ];
+    file = {
+      ".p10k.zsh".source = ../utils/p10k.zsh;
+    };
   };
 }
