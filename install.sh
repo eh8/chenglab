217b0a82598e515c46593e470c4d91cb68fed5b2
Eric Cheng <eh8@Erics-MacBook-Air.local>
2024-03-04 20:12:52 -0500

    fix: address installation dependency bugs

--- install.sh
+++ install.sh
@@ -10,11 +10,18 @@ if [ "$(uname)" == "Darwin" ]; then
 
   echo -e "\n\033[1mInstalling Nix...\033[0m"
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
+  echo -e "\n\033[1mInstalling Xcode...\033[0m"
+  xcode-select --install
+  
+  echo -e "\n\033[1mInstalling Rosetta...\033[0m"  
+  softwareupdate --install-rosetta
 
   # Completed
   echo -e "\n\033[1;32mAll steps completed successfully. nix-darwin is now ready to be installed.\033[0m\n"
-  echo -e "To install nix-darwin configuration for hostname, run the following command:\n"
-  echo -e "\033[1mnix run nix-darwin -- --flake github:eh8/chenglab#hostname\033[0m\n"
+  echo -e "To install nix-darwin configuration, run the following command:\n"
+  echo -e "\033[1mnix run nix-darwin -- switch --flake github:eh8/chenglab#mac1chng\033[0m\n"
   echo -e "Remember to add the new host public key to sops-nix!"
 elif [ "$(uname)" == "Linux" ]; then
   # Define disk
