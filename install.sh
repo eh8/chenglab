12deec88743381105b8fa1dfc352840934d9ea1e
Eric Cheng <eric@chengeric.com>
2024-03-04 20:46:33 -0500

    fix: adjust install.sh steps

--- install.sh
+++ install.sh
@@ -8,19 +8,19 @@ if [ "$(uname)" == "Darwin" ]; then
   echo -e "\n\033[1;31m**Warning:** This script will prepare system for nix-darwin installation.\033[0m"
   read -n 1 -s -r -p "Press any key to continue or Ctrl+C to abort..."
 
-  echo -e "\n\033[1mInstalling Nix...\033[0m"
-  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
-  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-
   echo -e "\n\033[1mInstalling Xcode...\033[0m"
   xcode-select --install
+
+  echo -e "\n\033[1mInstalling Nix...\033[0m"
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --no-confirm
   
   echo -e "\n\033[1mInstalling Rosetta...\033[0m"  
   softwareupdate --install-rosetta
 
   # Completed
   echo -e "\n\033[1;32mAll steps completed successfully. nix-darwin is now ready to be installed.\033[0m\n"
-  echo -e "To install nix-darwin configuration, run the following command:\n"
+  echo -e "To install nix-darwin configuration, run the following commands:\n"
+  echo -e "\033[1m. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh\033[0m\n"
   echo -e "\033[1mnix run nix-darwin -- switch --flake github:eh8/chenglab#mac1chng\033[0m\n"
   echo -e "Remember to add the new host public key to sops-nix!"
 elif [ "$(uname)" == "Linux" ]; then
