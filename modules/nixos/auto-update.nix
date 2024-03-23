dea793e5d09891cac057a739d0ad2e08b869c531
Eric Cheng <eric@chengeric.com>
2024-03-23 13:11:09 -0400

    test: daily autoupgrade

--- modules/nixos/auto-update.nix
+++ modules/nixos/auto-update.nix
@@ -2,7 +2,7 @@
   # inspo: https://github.com/reckenrode/nixos-configs/blob/main/hosts/meteion/configuration.nix
   system.autoUpgrade = {
     enable = true;
-    dates = "Sun *-*-* 07:00:00";
+    dates = "*-*-* 07:00:00";
     randomizedDelaySec = "1h";
     flake = "github:eh8/chenglab";
   };
