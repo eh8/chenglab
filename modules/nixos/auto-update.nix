bfa6a2b865972d11b8f6338e445ffdd005829f4d
Eric Cheng <eric@chengeric.com>
2024-03-23 03:58:35 -0400

    refactor: autoupgrade timer

--- modules/nixos/auto-update.nix
+++ modules/nixos/auto-update.nix
@@ -1,9 +1,9 @@
-{config, ...}: {
+{
   # inspo: https://github.com/reckenrode/nixos-configs/blob/main/hosts/meteion/configuration.nix
   system.autoUpgrade = {
     enable = true;
-    dates = "03:30";
-    # randomizedDelaySec = "45min";
+    dates = "Sun *-*-* 07:00:00";
+    randomizedDelaySec = "1h";
     flake = "github:eh8/chenglab";
   };
 }
