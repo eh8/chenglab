2b8c479bbaf3a46355663e8125cf962bbb98fb42
Eric Cheng <eric@chengeric.com>
2024-03-25 02:55:31 -0400

    refactor: simpler flake.nix

--- flake.nix
+++ flake.nix
@@ -53,36 +53,17 @@
     ...
   } @ inputs: let
     inherit (self) outputs;
+
     systems = [
       "x86_64-linux"
       "aarch64-darwin"
     ];
+
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     # Enables `nix fmt` at root of repo to format all nix files
     formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-    # Test bench desktop
-    nixosConfigurations = {
-      dsk1chng = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        modules = [./machines/dsk1chng/configuration.nix];
-      };
-    };
-
-    # Custom NixOS iso
-    nixosConfigurations = {
-      iso1chng = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs;};
-        system = "x86_64-linux";
-        modules = [
-          (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
-          ./machines/iso1chng/configuration.nix
-        ];
-      };
-    };
-
-    # M1 Macbook Air
     darwinConfigurations = {
       mac1chng = nix-darwin.lib.darwinSystem {
         specialArgs = {inherit inputs outputs;};
@@ -90,24 +71,31 @@
       };
     };
 
-    # Server 1
     nixosConfigurations = {
+      dsk1chng = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [./machines/dsk1chng/configuration.nix];
+      };
+
+      iso1chng = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+          ./machines/iso1chng/configuration.nix
+        ];
+      };
+
       svr1chng = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [./machines/svr1chng/configuration.nix];
       };
-    };
 
-    # Server 2
-    nixosConfigurations = {
       svr2chng = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [./machines/svr2chng/configuration.nix];
       };
-    };
 
-    # Server 3
-    nixosConfigurations = {
       svr3chng = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [./machines/svr3chng/configuration.nix];
