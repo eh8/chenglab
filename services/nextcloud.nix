5cc42df0fa44d26137d715f7239c70777501458e
Eric Cheng <eric@chengeric.com>
2024-10-14 02:09:41 -0400

    chore: nextcloud upgrade

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -17,7 +17,7 @@
   services = {
     nextcloud = {
       enable = true;
-      package = pkgs.nextcloud29;
+      package = pkgs.nextcloud30;
       hostName = "cloud.chengeric.com";
 
       https = true;
