a0b7309b92269bcc4011d4c754e89407ae1fcd2b
Eric Cheng <eric@chengeric.com>
2024-10-14 02:08:34 -0400

    chore: remove broken alias

--- modules/home-manager/_zsh.nix
+++ modules/home-manager/_zsh.nix
@@ -7,7 +7,6 @@
     shellAliases = {
       ".." = "cd ..";
       cat = "bat --style=plain --theme=base16 --paging=never ";
-      e = "eza ";
       neofetch = "fastfetch";
       sudo = "sudo ";
       v = "vim ";
