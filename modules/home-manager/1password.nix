e007beec6ea4a4e95a4b05758e6fb74133aa4a0f
Eric Cheng <eric@chengeric.com>
2025-03-19 03:19:06 -0400

    chore: simplify gpg signing across darwin/linux

--- modules/home-manager/1password.nix
+++ modules/home-manager/1password.nix
@@ -4,23 +4,24 @@
   vars,
   ...
 }: {
-  programs.ssh = {
-    enable = true;
-    extraConfig = lib.mkMerge [
-      (lib.mkIf pkgs.stdenv.isLinux ''
-        IdentityAgent "~/.1password/agent.sock"
-      '')
-      (lib.mkIf pkgs.stdenv.isDarwin ''
-        IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-      '')
-    ];
-  };
-
-  programs.git.extraConfig = {
-    gpg.ssh.program = lib.mkMerge [
-      (lib.mkIf pkgs.stdenv.isLinux "${pkgs._1password-gui}/bin/op-ssh-sign")
-      (lib.mkIf pkgs.stdenv.isDarwin "/Applications/1Password.app/Contents/MacOS/op-ssh-sign")
-    ];
+  programs = {
+    git.extraConfig = {
+      # inspo: https://wiki.nixos.org/wiki/1Password#Configuring_Git
+      "gpg \"ssh\"" = {
+        program = "${lib.getExe' pkgs._1password-gui "op-ssh-sign"}";
+      };
+    };
+    ssh = {
+      enable = true;
+      extraConfig = lib.mkMerge [
+        (lib.mkIf pkgs.stdenv.isLinux ''
+          IdentityAgent "~/.1password/agent.sock"
+        '')
+        (lib.mkIf pkgs.stdenv.isDarwin ''
+          IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+        '')
+      ];
+    };
   };
 
   xdg.configFile."1Password/ssh/agent.toml" = {
