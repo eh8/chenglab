27d07e76d7061202f27358b72994810e7713127f
Eric Cheng <eric@chengeric.com>
2025-06-03 22:58:48 -0400

    chore: switched ssh key

--- vars.nix
+++ vars.nix
@@ -4,5 +4,5 @@
   userName = "eh8";
   userEmail = "eric@chengeric.com";
   sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIkcgwjYMHqUDnx0JIOSXQ/TN80KEaFvvUWA2qH1AHFC";
-  sshPublicKeyWork = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIECqbKvyxrgINg2SQzK8jPZidqwkCi4UOKkPDlWKYBN3";
+  sshPublicKeyWork = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICoUSf5zC/frW48cnU3r4qYoP86iDNqqbu7A9iGek12a";
 }
