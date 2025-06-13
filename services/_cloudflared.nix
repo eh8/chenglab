4f787717be8135dadc2197642bf2851a8443a006
Eric Cheng <eric@chengeric.com>
2025-06-13 03:10:24 -0400

    fix: alternative dns check for cloudflare

--- services/_cloudflared.nix
+++ services/_cloudflared.nix
@@ -46,6 +46,9 @@
       wantedBy = ["default.target"];
       serviceConfig = {
         Type = "oneshot";
+        # workaround to ensure dns is available before setting up cloudflare tunnel
+        # inspo: chatgpt
+        ExecStartPre = "${pkgs.bash}/bin/bash -c 'for i in {1..10}; do ${pkgs.iputils}/bin/ping -c1 api.cloudflare.com && exit 0 || sleep 3; done; exit 1'";
         ExecStart = "${lib.getExe pkgs.cloudflared} tunnel route dns 'Chenglab-01' 'watch.chengeric.com'";
       };
     };
