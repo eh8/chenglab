4d09c952372c0fae8e026a70e9058869a72f1955
Eric Cheng <eric@chengeric.com>
2025-08-19 02:08:39 -0400

    test: remove drive

--- machines/svr2chng/hardware-configuration.nix
+++ machines/svr2chng/hardware-configuration.nix
@@ -13,15 +13,15 @@
       # `readlink /sys/class/net/enp0s31f6/device/driver` indicates "e1000e" is the ethernet driver for this device
       availableKernelModules = ["nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" "e1000e"];
       luks = {
-        reusePassphrases = true;
+        # reusePassphrases = true;
         devices = {
           "cryptroot" = {
             device = "/dev/nvme0n1p2";
             allowDiscards = true;
           };
-          "fun" = {
-            device = "/dev/sda1";
-          };
+          # "fun" = {
+          #   device = "/dev/sda1";
+          # };
         };
       };
     };
@@ -42,10 +42,10 @@
       device = "/dev/disk/by-label/nix";
       fsType = "ext4";
     };
-    "/fun" = {
-      device = "/dev/disk/by-label/fun";
-      fsType = "ext4";
-    };
+    # "/fun" = {
+    #   device = "/dev/disk/by-label/fun";
+    #   fsType = "ext4";
+    # };
   };
 
   networking.useDHCP = lib.mkDefault true;
