de5b8ea1c9c35a8c75082f6d10ae1a63fafdb1ab
Eric Cheng <eric@chengeric.com>
2025-09-11 14:27:33 -0400

    chore: packages

--- modules/wsl/_packages.nix
+++ modules/wsl/_packages.nix
@@ -1,10 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  pkgs-unstable = import inputs.nixpkgs-unstable {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+in {
   environment.systemPackages = with pkgs; [
     # inspo: https://mynixos.com/nixpkgs/package/azure-cli
-    (azure-cli.withExtensions [
-      azure-cli.extensions.k8s-extension
-      azure-cli.extensions.aks-preview
-      azure-cli.extensions.amg
+    (pkgs-unstable.azure-cli.withExtensions [
+      pkgs-unstable.azure-cli.extensions.k8s-extension
+      pkgs-unstable.azure-cli.extensions.aks-preview
+      pkgs-unstable.azure-cli.extensions.amg
     ])
     k9s
     kubectl
