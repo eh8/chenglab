27891865686c7bfff0c2b93515b57f8fc431ccd0
Eric Cheng <eric@chengeric.com>
2025-11-12 17:18:25 -0500

    chore: kubectl enter script

--- modules/wsl/base.nix
+++ modules/wsl/base.nix
@@ -42,6 +42,50 @@
   services.vscode-server.enable = true;
   time.timeZone = "America/New_York";
 
+  environment.systemPackages = [
+    # inspo: https://github.com/croomes
+    (pkgs.writeShellScriptBin "kubectl-enter" ''
+            if [ -z "$1" ]; then
+              echo "Please specify node name"
+              exit 1
+            fi
+
+            NODE="$1"
+            IMAGE="alpine"
+            POD="nsenter-$(env LC_CTYPE=C tr -dc a-z0-9 < /dev/urandom | head -c 6)"
+            NAMESPACE=""
+
+            kubectl get node "$NODE" >/dev/null || exit 1
+
+            OVERRIDES="$(
+              cat <<EOT
+      {
+        "spec": {
+          "nodeName": "$NODE",
+          "hostPID": true,
+          "containers": [
+            {
+              "securityContext": {
+                "privileged": true
+              },
+              "image": "$IMAGE",
+              "name": "nsenter",
+              "stdin": true,
+              "stdinOnce": true,
+              "tty": true,
+              "command": [ "nsenter", "--target", "1", "--mount", "--uts", "--ipc", "--net", "--pid", "--", "bash", "-l" ]
+            }
+          ]
+        }
+      }
+      EOT
+            )"
+
+            echo "spawning \"$POD\" on \"$NODE\""
+            kubectl run --namespace "$NAMESPACE" --rm --image "$IMAGE" --overrides="$OVERRIDES" -ti "$POD"
+    '')
+  ];
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
 }
