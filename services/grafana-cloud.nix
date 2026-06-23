9d1271df617f361b1cd580fce15a2c05f0eca96d
Eric Cheng <eric@chengeric.com>
2026-06-22 22:07:51 -0400

    chore: grafana cleanup

--- services/grafana-cloud.nix
+++ services/grafana-cloud.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  vars,
+  ...
+}: {
   sops = {
     secrets."grafana-cloud-token" = {};
 
@@ -102,7 +106,7 @@
         url = "https://prometheus-prod-66-prod-us-east-3.grafana.net/api/prom/push"
 
         basic_auth {
-          username = "3325626"
+          username = "${vars.grafanaCloudMetricsId}"
           password = sys.env("GRAFANA_CLOUD_TOKEN")
         }
       }
--- vars.nix
+++ vars.nix
@@ -5,7 +5,7 @@
   userEmail = "eric@chengeric.com";
   sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIkcgwjYMHqUDnx0JIOSXQ/TN80KEaFvvUWA2qH1AHFC";
   sshPublicKeyWork = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICoUSf5zC/frW48cnU3r4qYoP86iDNqqbu7A9iGek12a";
-  # note: optional Grafana Cloud Prometheus and Loki instance IDs
+
+  # note: optional Grafana Cloud Prometheus instance ID
   grafanaCloudMetricsId = "3325626";
-  grafanaCloudLogsId = "1658488";
 }
