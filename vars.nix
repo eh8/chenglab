bb12efbcd82caeec44f182cf6d08eb0aa39d76f4
Eric Cheng <eric.cheng@cerebras.net>
2026-07-01 18:11:32 -0400

    chore: work ssh key

--- vars.nix
+++ vars.nix
@@ -8,5 +8,5 @@
   timeZone = "America/New_York";
   renderDevice = "/dev/dri/renderD128";
   sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIkcgwjYMHqUDnx0JIOSXQ/TN80KEaFvvUWA2qH1AHFC";
-  sshPublicKeyWork = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICoUSf5zC/frW48cnU3r4qYoP86iDNqqbu7A9iGek12a";
+  sshPublicKeyWork = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7Ju7wpJpiW0D7kq3u9NMJ6M2L0q/IRhA5tNOaAnSdj";
 }
